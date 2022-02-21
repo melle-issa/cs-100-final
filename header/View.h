521408e68d6b7cfe9ff9c8a1b24c2b9c0f178a77
Evan Shimoniak <eshim009@ucr.edu>
2022-02-20 17:29:09 -0800

    Added View.h (with comments)

---
+++ header/View.h
@@ -0,0 +1,21 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+class View {
+    public:
+        /* Updates the display buffer with the most recent data from this view
+         * 
+         * - Should be called after changing something that this view displays
+         * - Note: The user's screen will not be updated until running refresh
+         */
+        virtual void draw() = 0;
+        
+        /* Copies the display buffer to the user's screen
+         * 
+         * - Anything added to the screen with draw should now be visible to the
+         *   user
+         */
+        virtual void refresh() = 0;
+}
+
+#endif // VIEW_H
